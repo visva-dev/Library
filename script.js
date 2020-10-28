f680ef631e2b03d6f2f4531d5ac8e0ed95668d29
Visvaldas <visva.rapalis@gmail.com>
2020-10-28 11:09:48 +0200

    Add html form and basic js

---
+++ script.js
@@ -0,0 +1,26 @@
+let myLibrary = [];
+
+
+function Book(title, author, pages) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  // this.sayName = function() {
+  //   console.log(name)
+  // }
+}
+
+function addBookToLibrary() {
+  // do stuff here
+  book = []
+  book.title.push()
+  let name = window.prompt("Enter your name: ");
+alert("Your name is " + name);
+}
+
+const player1 = new Player('steve', 'X')
+const player2 = new Player('also steve', 'O')
+player1.sayName() // logs 'steve'
+player2.sayName() // logs 'also steve'
+
+theHobbit.info() // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
